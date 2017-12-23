09910050c20d843c0858f6004881ccc35daf9c86
Kathy Hudson <khitx@icloud.com>
2017-12-23 19:09:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-board = ["X", "X", "X", "O", "X", "X", "O", "X", "O"]
+#board = ["X", "X", "X", "O", "X", "X", "O", "X", "O"]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |wci|
@@ -59,4 +59,4 @@ def winner(board)
   return board[wrow[0]]
 end
 
-puts(winner(board))
+#puts(winner(board))
