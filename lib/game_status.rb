6414686f2c556926048611d8d29acef1e97cd5a6
Kathy Hudson <khitx@icloud.com>
2017-12-23 17:40:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WC = [
   [2,4,6]
 ]
 
-board = ["", "", "", "X", "X", "X", " ", " ", " "]
+board = ["", "", "X", "X", "", "X", "X", "X", "X"]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |wci|
