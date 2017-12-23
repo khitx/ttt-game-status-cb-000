8aac5da69c4de0ec3f1989e870c69b36986201d7
Kathy Hudson <khitx@icloud.com>
2017-12-23 17:59:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WC = [
   [2,4,6]
 ]
 
-#board = ["O", "X", "X", "O", "", "X", "", "X", "O"]
+board = ["O", "X", "X", "O", "", "X", "", "X", "O"]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |wci|
@@ -39,7 +39,7 @@ def won?(board)
 end
 
 def full?(board)
-  puts(board.all? {|i| position_taken?(board,i)})
+  board.all? {|i| position_taken?(board,i)}
 end
 
-#puts(won?(board))
+puts(full?(board))
