45af7b32cb842a45061009beaa87c583c23853e1
Kathy Hudson <khitx@icloud.com>
2017-12-23 16:48:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,37 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+WC = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+board = ["X", "Y", "Z", " ", " ", " ", " ", " ", " "]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |wci|
+    wci.each do |i|
+      board[i].all? == 'X' || board[i].all? == 'O'
+      puts(board[i])
+    end
+  end
+end
+
+won?(board)
